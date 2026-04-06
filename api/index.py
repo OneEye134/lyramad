3e5850e6c11199af368f2a4819f98d62ac77a21e
OneEye134 <christianeduardalarcon@gmail.com>
2026-04-06 21:24:26 +0800

    made it better

--- api/index.py
+++ api/index.py
@@ -4,37 +4,36 @@ import os, json, collections
 app = Flask(__name__)
 BAD_DIR = './api/bad'
 
-# Load all JSON files
 def load_profanities():
     profanities = {}
     for filename in os.listdir(BAD_DIR):
         if filename.endswith('.json'):
             path = os.path.join(BAD_DIR, filename)
-            word = filename.split('_')[0]  # get "puta" from "puta_0832pm_april6.json"
+            word = filename.split('_')[0].lower()
             with open(path, 'r', encoding='utf-8') as f:
                 profanities[word] = json.load(f)
     return profanities
 
 profanities = load_profanities()
 
-# Homepage
+@app.context_processor
+def inject_categories():
+    return dict(categories=list(profanities.keys()))
+
 @app.route('/')
 def index():
-    categories = list(profanities.keys())
-    return render_template('index.html', categories=categories)
+    return render_template('index.html')
 
-# Search by category or word
 @app.route('/search', methods=['GET'])
 def search():
     query = request.args.get('q', '').lower()
     results = []
     for word, entries in profanities.items():
-        if query in word or query in ''.join([e['msg'].lower() for e in entries]):
+        if query in word or any(query in e['msg'].lower() for e in entries):
             for e in entries:
                 results.append({'word': word, **e})
     return render_template('search.html', query=query, results=results)
 
-# Leaderboard
 @app.route('/leaderboard')
 def leaderboard():
     counter = collections.Counter()
@@ -43,6 +42,17 @@ def leaderboard():
             counter[e['name']] += 1
     top_users = counter.most_common(20)
     return render_template('leaderboard.html', top_users=top_users)
+
+@app.route('/leaderboard/<word>')
+def word_leaderboard(word):
+    word = word.lower()
+    if word not in profanities:
+        return f"No data for {word}", 404
+    counter = collections.Counter()
+    for e in profanities[word]:
+        counter[e['name']] += 1
+    top_users = counter.most_common(20)
+    return render_template('leaderboard.html', top_users=top_users, word=word)
 
 if __name__ == '__main__':
     app.run(debug=True)
